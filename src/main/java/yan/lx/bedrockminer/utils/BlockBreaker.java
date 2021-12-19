3b4d67b3d5fdf74eea5f7fd195d43d712b60cfcd
thebest9178 <76120883+thebest9178@users.noreply.github.com>
2021-12-19 11:56:50 -0800

    Update BlockBreaker.java

--- src/main/java/yan/lx/bedrockminer/utils/BlockBreaker.java
+++ src/main/java/yan/lx/bedrockminer/utils/BlockBreaker.java
@@ -5,18 +5,10 @@ import net.minecraft.client.world.ClientWorld;
 import net.minecraft.item.Items;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
-//import net.minecraft.block.RedstoneTorchBlock;
-//import net.minecraft.util.math.Direction;
-
-//import java.util.ArrayList;
-
-//import static net.minecraft.block.Block.sideCoversSmallSquare;
 
 public class BlockBreaker {
     public static void breakBlock(ClientWorld world, BlockPos pos) {
         InventoryManager.switchToItem(Items.DIAMOND_PICKAXE);
         MinecraftClient.getInstance().interactionManager.attackBlock(pos, Direction.UP);
     }
-
-
 }
